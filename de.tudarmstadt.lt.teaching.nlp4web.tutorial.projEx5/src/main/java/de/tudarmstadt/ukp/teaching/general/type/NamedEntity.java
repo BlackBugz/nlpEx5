a4bf74091cda67ff1e177b134a706fcfd055c2c9
nigga-plz <negromarc@gmail.com>
2014-11-20 15:33:21 +0100

    Added Named Entity Annotation

---
+++ de.tudarmstadt.lt.teaching.nlp4web.tutorial.projEx5/src/main/java/de/tudarmstadt/ukp/teaching/general/type/NamedEntity.java
@@ -0,0 +1,101 @@
+
+
+/* First created by JCasGen Thu Nov 20 15:32:31 CET 2014 */
+package de.tudarmstadt.ukp.teaching.general.type;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Thu Nov 20 15:32:31 CET 2014
+ * XML source: /home/marco/Documenti/TUDa/NLP/Es5/nlpEx5/de.tudarmstadt.lt.teaching.nlp4web.tutorial.projEx5/src/main/resources/desc/type/NamedEntity.xml
+ * @generated */
+public class NamedEntity extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(NamedEntity.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected NamedEntity() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public NamedEntity(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public NamedEntity(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public NamedEntity(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: EntityType
+
+  /** getter for EntityType - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getEntityType() {
+    if (NamedEntity_Type.featOkTst && ((NamedEntity_Type)jcasType).casFeat_EntityType == null)
+      jcasType.jcas.throwFeatMissing("EntityType", "de.tudarmstadt.ukp.teaching.general.type.NamedEntity");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((NamedEntity_Type)jcasType).casFeatCode_EntityType);}
+    
+  /** setter for EntityType - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setEntityType(String v) {
+    if (NamedEntity_Type.featOkTst && ((NamedEntity_Type)jcasType).casFeat_EntityType == null)
+      jcasType.jcas.throwFeatMissing("EntityType", "de.tudarmstadt.ukp.teaching.general.type.NamedEntity");
+    jcasType.ll_cas.ll_setStringValue(addr, ((NamedEntity_Type)jcasType).casFeatCode_EntityType, v);}    
+  }
+
+    
