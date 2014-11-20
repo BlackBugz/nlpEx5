4f2d7037d396057f86091fea5b20ff23869f603e
Hendrik Luecketieke <hendrik.luecke-tieke@igd.fraunhofer.de>
2014-11-20 18:31:28 +0100

    Made log-level for NEConverter more coarsegrained to guarantee output

--- de.tudarmstadt.lt.teaching.nlp4web.tutorial.projEx5/src/main/java/de/tudarmstadt/lt/teaching/nlp4web/ml/reader/NamedEntityConverter.java
+++ de.tudarmstadt.lt.teaching.nlp4web.tutorial.projEx5/src/main/java/de/tudarmstadt/lt/teaching/nlp4web/ml/reader/NamedEntityConverter.java
@@ -105,7 +105,7 @@ public class NamedEntityConverter extends JCasAnnotator_ImplBase {
 				token.addToIndexes();
 				neTag.addToIndexes();
 				logger.log(
-						Level.FINE,
+						Level.INFO,
 						"Token: ["
 								+ docText.substring(token.getBegin(), token.getEnd()) 
 								+ "] \t"
